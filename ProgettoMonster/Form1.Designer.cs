459cbf238b05668be34d8447b910841da47601e5
Famiglia Albi <Famiglia Albi>
2017-02-19 21:10:48 +0100

    modifiche

---
+++ ProgettoMonster/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace ProgettoMonster
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(147, 105);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(194, 89);
+            this.textBox2.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(502, 325);
+            this.Controls.Add(this.textBox2);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ ProgettoMonster/Monster.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ProgettoMonster
+{
+    class Monster
+    {
+        public string name;
+        public int maxHp;
+        public int curHp;
+        public int damage;
+        public int healFactor;
+
+        public Monster(string name, int maxHp, int damage, int healFactor = 0)
+        {
+            this.name = name;
+            this.maxHp = maxHp;
+            curHp = maxHp;
+            this.damage = damage;
+            this.healFactor = healFactor;
+            describe();  //funzione
+        }
+
+        public string describe()  //inizializzo la funzione 
+        {
+            string output = "Questo è " + name + "\r\n";
+            output += "HP: " + curHp + "\r\n";
+            output += "DAMAGE: " + damage + "\r\n";
+
+            return output;
+        }
+
+        public void describe(TextBox t)    //passo la textbox come argomento 
+        {
+            t.Text = describe();
+        }
+
+        public void heal(Monster target)     //il target è colui che riceve la cura
+        {
+            if (healFactor == 0)
+            {
+                Console.WriteLine("Non hai il potere di curare nessuno");
+                return;
+            }
+
+            if (curHp <= 0)
+            {
+                Console.WriteLine("Non puoi curare nessuno da morto");   //sei morto e non puoi curare (hai la curHp minore di 0)
+                return;
+            }
+
+            if (target.curHp <= 0)
+            {
+                Console.WriteLine(target.name + " è esausto e non puoi resuscitarlo con la cura.");
+                return;
+            }
+
+            target.curHp += healFactor;
+
+            if (target.curHp > target.maxHp)
+            {
+                target.curHp = target.maxHp;
+            }
+
+            Console.WriteLine(name + " usa cura su " + target.name);
+            Console.WriteLine(target.name + " è stato curato e ora ha " + target.curHp + "/" + target.maxHp + " HP");
+        }
+
+        public void heal()
+        {
+            heal(this);
+        }
+
+        public void attack(Monster target)     //funzione attacco: il target è colui che viene attaccato 
+        {
+            if (curHp <= 0)
+            {
+                Console.WriteLine("Non puoi attaccare nessuno da morto");
+                return;
+            }
+
+            if (target.curHp <= 0)
+            {
+                Console.WriteLine(target.name + " è già esausto, non infierire.");
+                return;
+            }
+
+            Console.WriteLine(name + " attacca " + target.name);
+            Console.WriteLine(name + " fa " + damage + " danni a " + target.name);
+            target.curHp -= damage;
+
+            if (target.curHp <= 0)
+            {
+                target.curHp = 0;
+                Console.WriteLine(target.name + " è esausto.");
+            }
+            else
+            {
+                Console.WriteLine("a " + target.name + " rimangono " + target.curHp + " hp");
+            }
+        }
+    }
+}
+//febbraio
